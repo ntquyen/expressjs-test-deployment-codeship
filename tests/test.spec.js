4f8b99cee2bba5979ac0701b228fc1a0b88256c3
ntquyen <nguyen.tuan.quyen.it@gmail.com>
2014-05-30 10:33:38 +0700

    First commit

---
+++ tests/test.spec.js
@@ -0,0 +1,12 @@
+
+describe('Test', function() {
+  before(function(done) {
+    done();
+  });
+  after(function(done) {
+    done();
+  });
+  it('Should do the test', function(done) {
+    done();
+  })
+});
